eb47e1cc574b86ab101f79a8970533846e8753d6
Tut-k0 <Tut_k0@protonmail.com>
2022-01-19 23:56:08 -0600

    Updating width of listbox.

--- books.py
+++ books.py
@@ -81,7 +81,7 @@ isbn_text = StringVar()
 e4 = Entry(window, textvariable=isbn_text)
 e4.grid(row=1, column=3)
 
-list1 = Listbox(window, height=6, width=35)
+list1 = Listbox(window, height=6, width=40)
 list1.grid(row=2, column=0, rowspan=6, columnspan=2)
 
 sb1 = Scrollbar(window)
